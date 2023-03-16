ae669a6a0bcf3cc1b4f582acaf5334582c60f7c2
BeastBeastov <7710963@mail.ru>
2023-03-16 23:33:42 +0300

    v 0.0.3 версия для телефона

--- main.py
+++ main.py
@@ -11,28 +11,44 @@ Created on Mon Feb 13 00:26:50 2023
 """
 
 from workmod import *
-
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
+from kivy.graphics import Line, Color, Ellipse
+from random import randint
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
-
-
-# Разрешение экрана телефона 1440х3200
-# Window.size = (288, 540)
-
 from kivy.config import Config
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
-# from kivymd.theming import ThemeManager
+# Разрешение экрана телефона 1440х3200
+# Window.size = (360, 700)
+
+class PainterWidget(Widget):
+    def on_touch_down(self, touch):
+        with self.canvas:
+            r = randint(0, 25) / 25
+            g = randint(0, 25) / 25
+            b = randint(0, 25) / 25
+            a = 1
+            Color(r, g, b, a)
+            r = 20
+            Ellipse(pos=(touch.x - r / 2, touch.y - r / 2), size=(r, r))
+            touch.ud['line'] = Line(points=(touch.x, touch.y), width=10)
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += (touch.x, touch.y)
+    def clear_canvas(self):
+        self.canvas.clear()
+    def save(self):
+        self.size = (Window.size[0], Window.size[1])
+        self.export_to_png('image.png')
 
 class Container(ScreenManager):
 
-    login = ObjectProperty()
-    pwd = ObjectProperty()
+
     def calc_formwork_materials(self):
-        l = float(self.length1.text)
-        w = float(self.width1.text)
+        l = float(self.length1.text) if self.length1.text else 0
+        w = float(self.width1.text) if self.width1.text else 0
         P = 2 * l + 2 * w
         M = formwork_materials(P)
         self.perimeter.text = str(P) + ' п.м.'
@@ -42,9 +58,10 @@ class Container(ScreenManager):
         self.nuts.text = str(M[3][0])+' шт.\n'+str(M[3][1])+' кг.'
         self.washers.text = str(M[4][0])+' шт.\n'+str(M[4][1])+' кг.'
         self.screws.text = str(M[5])+' шт.'
+
     def calc_local_materials(self):
-        l = float(self.length2.text)
-        w = float(self.width2.text)
+        l = float(self.length2.text) if self.length2.text else 0
+        w = float(self.width2.text) if self.width2.text else 0
         M = local_materials(l, w)
         self.concrete.text = str(M[0])+' куб.м.'
         self.steel.text = str(M[1])+' п.м.\n'+str(M[2])+' п.м.'
@@ -55,73 +72,62 @@ class Container(ScreenManager):
         self.putty.text = str(M[7])+' меш.'
         self.glue.text = str(M[8])+' меш.'
 
-
     def start(self):
+        self.length1.text = ''
+        self.width1.text = ''
+        self.length2.text = self.width2.text = ''
         self.login.text = ''
         self.login.hint_text = 'логин'
         self.pwd.text = ''
         self.pwd.hint_text = 'пароль'
         self.current = 'login'
         self.transition.direction = 'right'
-        # self.switch_to('manu', direction='left')
-        # print('Начало')
+
     def startlogin(self):
-        if self.login.text == 'admin' and self.pwd.text == 'admin':
+        if self.login.text == '' and self.pwd.text == '':
             self.current = 'menu'
             self.transition.direction = 'left'
-            # print('Выполняется вход : ' + self.login.text)
-        elif self.login.text == '' and self.pwd.text == '':
-            self.current = 'menu'
-            self.transition.direction = 'left'
-            # print('Выполняется вход без пароля!')
         else:
             pass
     def settings(self):
         self.current = 'settings'
-        self.transition.direction = 'left'
-        # print('Настройки')
+        self.transition.direction = 'right'
     def menu(self):
         self.current = 'menu'
         self.transition.direction = 'right'
-        # print('Меню')
     def profile(self):
         self.current = 'profile'
-        self.transition.direction = 'right'
-        # print('Профиль')
+        self.transition.direction = 'left'
     def calcs(self):
         self.current = 'calcs'
         self.transition.direction = 'left'
-        # print('Рассчеты')
     def back_to_calcs(self):
         self.current = 'calcs'
         self.transition.direction = 'right'
-        # print('Рассчеты')
     def on_login(self, instance, *args):
         self.login.text = ''
     def on_pwd(self, instance, *args):
         self.pwd.text = ''
     def projects(self):
-        # print('Проекты')
+        pass
+    def painter(self):
+        self.current = 'painter'
+        self.transition.direction = 'left'
     def form_mc(self):
         self.current = 'form_mc'
         self.transition.direction = 'left'
-        # print('Расчет материалов опалубки')
     def local_mc(self):
         self.current = 'local_mc'
         self.transition.direction = 'left'
-        # print('Расчет местных стройматериалов')
     def face_mc(self):
-        # print('Расчет облицовочных материалов')
+        pass
     def wsc(self):
-        # print('Расчет работ и услуг')
+        pass
+
 
 class PoolToolApp(App):
-    #theme_cls = ThemeManager()
-    #title = 'PoolTool'
     def build(self):
-        # self.theme_cls.theme_style = 'Light'
         return Container()
-
 
 if __name__ == '__main__':
     PoolToolApp().run()
